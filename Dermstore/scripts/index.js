47128550d7e77a4168bdb1665a77ea62acde2566
HP <lokeshgola121@gmail.com>
2022-10-02 19:31:11 +0530

    day-5 work

--- Dermstore/scripts/index.js
+++ Dermstore/scripts/index.js
@@ -51,40 +51,39 @@
     dots[slideIndex-1].className += " active";
     }
 
-
    // Bestsellers
 let array=[ { 
-    imageLink: "https://static.thcdn.com/images/small/webp//productimg/original/11289609-8744892770898772.jpg",
-    para: "SkinCeuticals C E Ferulic (1 fl. oz.)",
+    image: "https://static.thcdn.com/images/small/webp//productimg/original/11289609-8744892770898772.jpg",
+    description: "SkinCeuticals C E Ferulic (1 fl. oz.)",
     border_para: "$22 SkinCeuticals Gift",
     price: "$169.00",
 },
 {
-    imageLink: "https://static.thcdn.com/images/small/webp//productimg/original/13315093-7444974668249790.jpg",
-    para: "SkinCeuticals C E Ferulic (1 fl. oz.)",
+    image: "https://static.thcdn.com/images/small/webp//productimg/original/13315093-7444974668249790.jpg",
+    description: "SkinCeuticals C E Ferulic (1 fl. oz.)",
     border_para: "$22 SkinCeuticals Gift",
     price: "$165.00",
 },
 {
-    imageLink: "https://static.thcdn.com/images/small/webp//productimg/original/13812005-4724980887145978.jpg",
-    para: "SkinCeuticals C E Ferulic (1 fl. oz.)",
+    image: "https://static.thcdn.com/images/small/webp//productimg/original/13812005-4724980887145978.jpg",
+    description: "SkinCeuticals C E Ferulic (1 fl. oz.)",
     border_para: "$22 SkinCeuticals Gift",
     price: "$160.00",
 },
 {
-    imageLink: "https://static.thcdn.com/images/small/webp//productimg/original/11291543-2074864291141689.jpg",
-    para: "SkinCeuticals C E Ferulic (1 fl. oz.)",
+    image: "https://static.thcdn.com/images/small/webp//productimg/original/11291543-2074864291141689.jpg",
+    description: "SkinCeuticals C E Ferulic (1 fl. oz.)",
     border_para: "$22 SkinCeuticals Gift",
     price: "$169.00",
 },
 {
-    imageLink: "https://static.thcdn.com/images/small/webp//productimg/original/12664555-1704871739992403.jpg",
-    para: "SkinCeuticals C E Ferulic (1 fl. oz.)",
+    image: "https://static.thcdn.com/images/small/webp//productimg/original/12664555-1704871739992403.jpg",
+    description: "SkinCeuticals C E Ferulic (1 fl. oz.)",
     border_para: "$22 SkinCeuticals Gift",
     price: "$165.00",
 },{
-    imageLink: "https://static.thcdn.com/images/small/webp//productimg/original/11290635-9174978287699645.jpg",
-    para: "SkinCeuticals C E Ferulic (1 fl. oz.)",
+    image: "https://static.thcdn.com/images/small/webp//productimg/original/11290635-9174978287699645.jpg",
+    description: "SkinCeuticals C E Ferulic (1 fl. oz.)",
     border_para: "$22 SkinCeuticals Gift",
     price: "$160.00",
 }
@@ -95,10 +94,10 @@ let array=[ {
     arr.forEach(function(el){
 
         let image = document.createElement('img');
-        image.src=el.imageLink
+        image.src=el.image
 
         let Para = document.createElement('p');
-       Para.innerText=el.para
+       Para.innerText=el.description
 
         let paraWithBorder = document.createElement('p');
         paraWithBorder.innerText=el.border_para
@@ -114,18 +113,19 @@ let array=[ {
 
         let btn = document.createElement('button');
         btn.innerText="QUICK BUY"
+        btn.addEventListener("click",function(){
+            AddToCart(el)
+        })  
 
         let boxDiv = document.createElement('div')      
         boxDiv.append(image,Para,borderDiv,rating,Price,btn);
 
        document.querySelector('#bestSellerDivs').append( boxDiv);
-    //    console.log(boxDiv)
+    
     })
       
    }
-
 //    2nd slide code starts here
-
      slideIndex = 1;
     ShowSlides(slideIndex);
 
@@ -153,3 +153,56 @@ let array=[ {
         slides[slideIndex-1].style.display = "block";
         dots[slideIndex-1].className += " active";
     } 
+   
+    let btn = document.querySelectorAll('.addToCart');
+
+    let dataArr=[
+        
+               {
+                image:"https://static.thcdn.com/images/small/webp//productimg/original/11291496-1824871578503201.jpg" ,
+                description: "EltaMD UV Clear Broad-Spectrum SPF 44 (1.8 oz.)" ,
+                rating: "****4611",
+                price: "$39.00",
+               },
+               {
+                image: "https://static.thcdn.com/images/small/webp//productimg/original/13908285-1764980628345956.jpg",
+                description: "Total Protection™ Face Shield SPF 50 (PA+++) (1.8 fl. oz.)",
+                rating: "****2645",
+                price: "$59.00",
+               },
+               {
+                image: "https://static.thcdn.com/images/small/webp//productimg/original/11291965-1894871578716777.jpg" ,
+                description: "EltaMD UV Clear Broad-Spectrum SPF 36 (1.5 oz.)",
+                rating: "****3621" ,
+                price: "$79.00",
+               },
+               {
+                image: "https://static.thcdn.com/images/small/webp//productimg/original/11709474-2084866361945395.jpg",
+                description: "Mosco MD UV Clear Broad-Spectrum SPF 55 (1.4 oz.)" ,
+                rating: "****46" ,
+                price: "$99.00" ,
+               }
+  ];
+
+    btn.forEach(function (el,i){
+        el.addEventListener("click",function(){
+            addToCart(el,i)
+        });
+    })
+    let cartArr =JSON.parse(localStorage.getItem("add_to_cart")) || [];  // remember it to resolve it later
+   function addToCart(el,i){    
+        dataArr.forEach(function(elem,index){
+            if(i===index)
+            cartArr.push(elem)
+        });
+        localStorage.setItem("add_to_cart",JSON.stringify(cartArr)) 
+   }
+   function AddToCart(el){
+    cartArr.push(el)
+    localStorage.setItem("add_to_cart",JSON.stringify(cartArr)) 
+   }
+
+
+
+
+
